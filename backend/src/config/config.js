d0caa66c51d3456819995f07b05d91304021217a
THANH290803 <phucthanh290803@gmail.com>
2026-01-25 00:15:13 +0700

    Fix bug deploy

---
+++ backend/src/config/config.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    timezone: process.env.DB_TIMEZONE || "+07:00",
+    logging: false,
+  },
+
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    timezone: "+07:00",
+    logging: false,
+  },
+};
